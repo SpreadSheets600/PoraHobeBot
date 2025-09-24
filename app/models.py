615e7f501816f3897167918eb595644e36234b07
Soham Maity <sohammaity006@gmail.com>
2025-09-24 20:47:29 +0530

    feat: add User and OAuth models for user authentication

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from flask_login import UserMixin
+from . import db
+from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+class OAuth(OAuthConsumerMixin, db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = db.relationship(User)
