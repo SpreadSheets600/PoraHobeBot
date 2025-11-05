97ea81cdef9956b34201947bb360b29ce0fb5dbd
Soham Maity <sohammaity239@gmail.com>
2025-11-05 20:51:03 +0530

    feat: update redirect location in frontpage handler

--- api/frontpage.js
+++ api/frontpage.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.writeHead(302, {
-    Location: "http://dono-03.danbot.host:2521/frontpages",
+    Location: "http://budget01.iccnex.ovh:25589/frontpages",
   });
   res.end();
 }
